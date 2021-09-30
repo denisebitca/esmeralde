0c44cb20e00d22abe60ad38a6c3d29d4abcbc86e
Rafael Bitca <rafael.bitca@universite-paris-saclay.fr>
2021-09-30 20:20:33 +0200

    Added features for first years and APP students

--- src/commands/edt.ts
+++ src/commands/edt.ts
@@ -10,19 +10,32 @@ dayjs.extend(weekOfYear);
 import { Command } from '../index';
 
 interface gn {
-	[pName:string]: Array<number>
+	[pName:string]: number
 }
 let groupeNum : gn = {
-    "TP 3A1": [0,0],
-    "TP 3A2": [0,1],
-    "TP 3B1": [1,0],
-    "TP 3B2": [1,1],
-    "TP 3C1": [2,0],
-    "TP 3C2": [2,1],
-    "TP 3D1": [3,0],
-    "TP 3D2": [3,1]
+    "TP 3A1": 15824546,
+    "TP 3A2": 15824547,
+    "TP 3B1": 15824548,
+    "TP 3B2": 15824760,
+    "TP 3C1": 15824550,
+    "TP 3C2": 15824759,
+    "TP 3D1": 15824552,
+    "TP 3D2": 15824756,
+	"TP-1A": 15824504,
+	"TP-1B": 15824505,
+	"TP-1C": 15824506,
+	"TP-1D": 15824507,
+	"TP-1E": 15824508,
+	"TP-1F": 15824509,
+	"TP-1G": 15824510,
+	"TP-1H": 15824511,
+	"TP-1I": 15824807,
+	"TP-1K": 15824514,
+	"TP-1L": 15824828,
+	"TP-1M": 15824516,
+	"edt-g1": 15824574,
+	"edt-g2": 15824575
 }
-const groupeCode = [[15824546, 15824547], [15824548, 15824760], [15824550, 15824759], [15824552, 15824756]]
 
 function getEDTLink(name : string | number) : string{
     let now = dayjs()
@@ -38,9 +51,8 @@ function getEDTLink(name : string | number) : string{
     }
 	if(name){
 		if(typeof(name) === "string"){
-			let pos = groupeNum[name];
-    		let code = groupeCode[pos[0]][pos[1]];
-    		return "https://edt.iut-orsay.fr/vue_invite_horizontale.php?current_year=" + year + "&current_week=" + week + "&selec_groupe=27785342&groupes_multi%5B%5D=" + code + "&lar=1920&hau=1200"
+			let code = groupeNum[name];
+    		return "https://edt.iut-orsay.fr/vue_invite_horizontale.php?current_year=" + year + "&current_week=" + week + "&groupes_multi%5B%5D=" + code + "&lar=1920&hau=1200"
 		} else if(typeof(name) === "number"){
 			return "https://edt.iut-orsay.fr/vue_etudiant_horizontale.php?current_year=" + year + "&current_student=" + name + "&current_week=" + week + "&lar=1920&hau=1200"
 		}
@@ -62,12 +74,23 @@ let obj : Command = {
 		let test2 = await interaction.client.channels.fetch(test);
 		if(test2 !== undefined){
 			if(test2?.isText()){
-				if(test2 instanceof TextChannel && test2.name === "edt"){
-					let test3 : TextChannel = test2;
-					//get category name
-					let test4 = test3.guild.channels.cache.find(u => u.id == test3.parentId);
-					if(test4 != undefined){
-						return interaction.reply(getEDTLink(test4.name))
+				if(test2 instanceof TextChannel){
+					if(test2.name === "edt"){
+						let test3 : TextChannel = test2;
+						//get category name
+						let test4 = test3.guild.channels.cache.find(u => u.id == test3.parentId);
+						if(test4 != undefined){
+							return interaction.reply(getEDTLink(test4.name))
+						}
+					} else if (test2.name === "edt-g1" || test2.name === "edt-g2") {
+						let test3 : TextChannel = test2;
+						//get category name
+						let test4 = test3.guild.channels.cache.find(u => u.id == test3.parentId);
+						if(test4 != undefined){
+							if(test4.name === "ALTERNANCE"){
+								return interaction.reply(getEDTLink(test2.name));
+							}
+						}
 					}
 				}
 			}
